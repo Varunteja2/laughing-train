eb0890634066e633991e2b474e7ca0c448ad0ace
varunteja2 <varuntej651@gmail.com>
2023-03-05 16:51:29 +0530

    v

--- MAIN.py
+++ MAIN.py
@@ -50,7 +50,13 @@ class WeatherApp:
         self.city_name = city_name
         self.get_current_weather_data()
         self.get_forecast_data()
+
+    def display_weather_data(self):
+        root = Tk()
+        root.title(f"Weather in {self.city_name}")
+        root.geometry("400x400")
         
+
     
 
 
