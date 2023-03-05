ffa8d474796a5b9328f48d2d3d6c88d6a9168917
varunteja2 <varuntej651@gmail.com>
2023-03-05 16:14:48 +0530

    v

--- MAIN.py
+++ MAIN.py
@@ -9,4 +9,10 @@ class WeatherApp:
         self.city name = None
         self.weather_description = None
         self.forecast_data = None
-        
+
+    
+    def get_current_weather_data(self):
+        weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city_name}&appid={self.api_key}"
+        response = requests.get(weather_url)
+        if response.status_code !=200:
+            
