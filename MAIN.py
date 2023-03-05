f92dfd12c6c63ab66643c27ff29280a8934ce523
varunteja2 <varuntej651@gmail.com>
2023-03-05 16:42:45 +0530

    v

--- MAIN.py
+++ MAIN.py
@@ -34,6 +34,10 @@ class WeatherApp:
             date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
               if date.hour == 12:
                 temperature = round(forecast["main"]["temp"] - 273.15, 2)
+                 weather_description = forecast["weather"][0]["description"].capitalize()
+                weather_icon = forecast["weather"][0]["icon"]
+                
+
 
 
  
