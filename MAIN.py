4a801fb42d321f6ee1b6e7a95e35cb5f30cf0634
varunteja2 <varuntej651@gmail.com>
2023-03-05 15:55:48 +0530

    V

--- MAIN.py
+++ MAIN.py
@@ -4,4 +4,5 @@ from datetime import datetime, timedelta
 from tkinter import Tk, Label, PhotoImage, messagebox
 
 class WeatherApp:
-    def varun:
+    def _init_(self, api_key):
+        self.api_key = api_key
